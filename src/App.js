1c1f8c5294fa4d9faf650437d85583587da3cf9e
Brandon Lee <brandon.sangmin.lee@gmail.com>
2023-12-28 15:08:13 -0600

    Updated build for new SSL security updates

---
+++ src/App.js
@@ -0,0 +1,185 @@
+import $ from "jquery";
+import React from "react";
+import createClass from 'create-react-class';
+
+/* Weather */
+const apiKey = "9237036ab7c0bf80dd4223ff17715372";
+const icons = {
+    "01d":"sun--v1",
+    "01n":"bright-moon",
+    "02d":"partly-cloudy-day--v1",
+    "02n":"partly-cloudy-night",
+    "03d":"cloud",
+    "03n":"cloud",
+    "04d":"cloud",
+    "04n":"cloud",
+    "09d":"rain",
+    "09n":"rain",
+    "10d":"rain",
+    "10n":"rain",
+    "11d":"storm--v1",
+    "11n":"storm--v1",
+    "13d":"snow-storm",
+    "13n":"snow-storm",
+    "50d":"foggy-night-1",
+    "50n":"foggy-night-1"
+}
+function kToF(tempK) {
+    return Math.round((tempK-273.15)*1.8+32);
+}
+function weatherUpdate() {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?id=5102922&appid=${apiKey}`)
+  .then(function(resp) { return resp.json() })
+  .then(function(data) {
+      localStorage.setItem("description-today", data.weather[0].description);
+      localStorage.setItem("current-temp-today", kToF(data.main.temp));
+      localStorage.setItem("high-temp-today", kToF(data.main.temp_max));
+      localStorage.setItem("low-temp-today", kToF(data.main.temp_min));
+      localStorage.setItem("icon-today", "https://img.icons8.com/plasticine/400/000000/" + icons[data.weather[0].icon] + ".png");
+  });
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?id=5102922&appid=${apiKey}`)
+  .then(function(resp) { return resp.json() })
+  .then(function(data) {
+      localStorage.setItem("precipitation-today", data.list[0].pop * 100);
+
+      localStorage.setItem("description-tomorrow", data.list[8].weather[0].description);
+      localStorage.setItem("average-temp-tomorrow", kToF(data.list[8].main.temp));
+      localStorage.setItem("high-temp-tomorrow", kToF(data.list[8].main.temp_max));
+      localStorage.setItem("low-temp-tomorrow", kToF(data.list[8].main.temp_min));
+      localStorage.setItem("precipitation-tomorrow", data.list[8].pop * 100);
+      localStorage.setItem("icon-tomorrow", "https://img.icons8.com/plasticine/400/000000/" + icons[data.list[8].weather[0].icon] + ".png");
+  });
+}
+function weatherRender() {
+    $("#description-today").html(localStorage.getItem("description-today"));
+    $("#current-temp-today").html(localStorage.getItem("current-temp-today"));
+    $("#high-temp-today").html(localStorage.getItem("high-temp-today"));
+    $("#low-temp-today").html(localStorage.getItem("low-temp-today"));
+    $("icon-today").attr("src", localStorage.getItem("icon-today"));
+    $("#precipitation-today").html(localStorage.getItem("precipitation-today"));
+
+    $("#description-tomorrow").html(localStorage.getItem("description-tomorrow"));
+    $("#average-temp-tomorrow").html(localStorage.getItem("average-temp-tomorrow"));
+    $("#high-temp-tomorrow").html(localStorage.getItem("high-temp-tomorrow"));
+    $("#low-temp-tomorrow").html(localStorage.getItem("low-temp-tomorrow"));
+    $("#precipitation-tomorrow").html(localStorage.getItem("precipitation-tomorrow"));
+    $("#icon-tomorrow").attr("src", localStorage.getItem("icon-tomorrow"));
+}
+
+var App = createClass( {
+  getInitialState: function() {
+      return {page: "home"};
+  },
+
+  componentDidMount: function() {
+      weatherUpdate();
+      setInterval(weatherRender, 100);
+  },
+
+  render: function() {
+    // prep text
+    var schedule = localStorage.getItem("schedule");
+    var todo = localStorage.getItem("todo");
+    var notes = localStorage.getItem("notes");
+    // render
+    if (this.state.page == "home") {
+      return (
+        <div id="content-container">
+          <div id="app-container">
+              <div id="quicklinks" class="app-double">
+                  <h2 style={{backgroundColor: "var(--highlighter1)"}}>Quick Links</h2>
+                  <div id="quicklinks-container">
+                      <a href="https://www.gmail.com" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000/gmail.png" style={{width:"100px"}}/>
+                          <figcaption>Mail</figcaption>
+                      </a>
+                      <a href="https://www.drive.google.com" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000/google-drive.png" style={{width:"100px"}}/>
+                          <figcaption>Drive</figcaption>
+                      </a>
+                      <a href="https://calendar.google.com/" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000/google-calendar.png" style={{width:"100px"}}/>
+                          <figcaption>Calendar</figcaption>
+                      </a>
+                      <a href="https://phubprod.princeton.edu/psp/phubprod/?cmd=start" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000/fox.png" style={{width:"100px"}}/>
+                          <figcaption>TigerHub</figcaption>
+                      </a>
+                      <a href="https://play.spotify.com/" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000/spotify.png" style={{width:"100px"}}/>
+                          <figcaption>Spotify</figcaption>
+                      </a>
+                      <a href="https://github.com/" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000/github--v1.png" style={{width:"100px"}}/>
+                          <figcaption>GitHub</figcaption>
+                      </a>
+                      <a href="https://blackboard.princeton.edu/" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000  /compose.png" style={{width:"100px"}}/>
+                          <figcaption>BlackBoard</figcaption>
+                      </a>
+                      <a href="https://canvas.princeton.edu/" class="link-icon">
+                          <img src="https://img.icons8.com/plasticine/400/000000/canvas-student.png" style={{width:"100px"}}/>
+                          <figcaption>Canvas</figcaption>
+                      </a>
+                  </div>
+              </div>
+              <div id="todo" class="app-double">
+                  <h2 style={{backgroundColor: "var(--highlighter3)"}}>To do</h2>
+                  <textarea id="todo-area" style={{width:"100%", height:"75%"}}>{todo}</textarea>
+              </div>
+              <div id="schedule" class="app">
+                  <h2 style={{backgroundColor: "var(--highlighter2)"}}>Schedule</h2>
+                  <textarea id="schedule-area" style={{width:"100%", height:"75%"}}>{schedule}</textarea>
+              </div>
+              <div id="notes" class="app">
+                  <h2 style={{backgroundColor: "var(--highlighter4)"}}>Notes</h2>
+                  <textarea id="notes-area" style={{width:"100%", height:"75%"}}>{notes}</textarea>
+              </div>
+              <div id="weather" class="app-double">
+                  <h2 style={{backgroundColor: "var(--highlighter5)"}}>Weather</h2>
+                  <div style={{display:"flex", flexDirection:"row", fontSize:"x-large", fontWeight:"lighter", height:"75%"}}>
+                      <div style={{width:"400px", display:"flex", marginLeft:"60px", marginTop:"20px"}}>
+                          <p>
+                              <b>- TODAY -</b><br/>
+                              <span id="description-today"></span><br/>
+                              Current: <span id="current-temp-today"></span> &deg;F<br/>
+                              High: <span id="high-temp-today"></span> &deg;F<br/>
+                              Low: <span id="low-temp-today"></span> &deg;F<br/>
+                              Precipitation: <span id="precipitation-today"></span>%
+                          </p>
+                          <img id="icon-today" src="https://img.icons8.com/plasticine/400/000000/sun--v1.png" style={{height:"100px", margin:"10px"}}/>
+                      </div>
+                      <div style={{width:"400px", display:"flex", marginTop:"20px"}}>
+                          <p>
+                              <b>- TOMORROW -</b><br/>
+                              <span id="description-tomorrow"></span><br/>
+                              Current: <span id="average-temp-tomorrow"></span> &deg;F<br/>
+                              High: <span id="high-temp-tomorrow"></span> &deg;F<br/>
+                              Low: <span id="low-temp-tomorrow"></span> &deg;F<br/>
+                              Precipitation: <span id="precipitation-tomorrow"></span>%
+                          </p>
+                          <img id="icon-tomorrow" src="https://img.icons8.com/plasticine/400/000000/sun--v1.png" style={{height:"100px", margin:"10px"}}/>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div id="todiary" class="navigator" onClick={() => this.setState({page: "diary"})}>&#12297;</div>
+        </div>
+      );
+    } else {
+      var diary = localStorage.getItem("diary");
+      return (
+        <div id="content-container">
+          <div id="tohome" class="navigator" onClick={() => this.setState({page: "home"})}>&#12296;</div>
+          <div class="app-container">
+              <div class="app" style={{width: "80vw", height: "65vh"}}>
+                  <textarea id="diary-area" style={{width:"100%", height:"100%"}}>{diary}</textarea>
+              </div>
+          </div>
+        </div>
+      )
+    }
+  }
+} );
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,69 @@
+import $ from "jquery";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/main.css';
+import './css/themeswitcher.css';
+import App from './App';
+import DateComponent from './DateComponent';
+
+/* React updater */
+ReactDOM.render(
+  <React.StrictMode>
+    <div id="header-container">
+        <DateComponent/>
+        <div id="logo">&sect; leatherbound</div>
+    </div>
+    <App/>
+    <div id="footer-container">
+        <p>&#169; Brandon Lee, 2021 &#8226; all rights reserved</p>
+        <div style={{display:"flex", flexDirection: "row", alignItems: "center"}}>
+            <div class="theme-switch-wrapper">
+                <label class="theme-switch" for="checkbox">
+                    <input type="checkbox" id="checkbox" />
+                    <div class="slider round"></div>
+                </label>
+            <div style={{fontStyle: "normal"}}>&ensp;&#9790;</div>
+            </div>
+            <div><a href="http://brandonssandbox.com/">BrandonsSandbox.com</a></div>
+        </div>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root'),
+  function() {
+    window.onunload = function() {saveAreas();}
+    window.onkeypress = function() {saveAreas();}
+    window.onkeydown = function() {saveAreas(); console.log("saved");}
+  }
+);
+
+/* Load and save areas */
+function saveAreas(){
+  var names = ["schedule", "todo", "notes", "diary"];
+  for (var i = 0; i < 5; i++){
+      var area = document.getElementById(names[i] + "-area");
+      if (area) localStorage.setItem(names[i], area.value);
+  }
+}
+
+/* Theme manager */
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {        
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
